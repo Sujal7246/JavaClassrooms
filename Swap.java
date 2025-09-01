29588aedb07c22c9a8749ea6a98471c0416a1ca4
Sujal7246 <sujal7246.ca22@chitkara.edu.in>
2025-09-01 10:49:21 +0530

    second commit

---
+++ Swap.java
@@ -0,0 +1,13 @@
+public class Swap {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        
+        System.out.println("before swap:"+"a:"+a+"b:"+b);
+        int c=a;
+        a=b;
+        b=c;
+         System.out.println("after swap:"+"a:"+a+"b:"+b+"c:"+c);
+    }
+    
+}
