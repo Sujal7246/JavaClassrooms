04212229e56780de38aa947f92ea71098f43cd0e
Sujal7246 <sujal7246.ca22@chitkara.edu.in>
2025-09-01 10:32:06 +0530

    first commit

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String[] args) {
+    
+        int intVar = 100000;
+        System.out.println("int value: " + intVar);
+        
+        float floatVar = 3.14f;
+        System.out.println("float value: " + floatVar);
+
+        double doubleVar = 3.14159265359;
+        System.out.println("double value: " + doubleVar);
+
+        char charVar = 'A';
+        System.out.println("char value: " + charVar);
+
+        boolean boolVar = true;
+        System.out.println("boolean value: " + boolVar);
+    }
+}
+
